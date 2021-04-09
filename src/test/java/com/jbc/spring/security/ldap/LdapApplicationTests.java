a836344b8da6e970e833f58a85b5082b97259e8e
faysal <mfh.faysal.0@gmail.com>
2021-04-09 11:31:27 +0600

    Project created

---
+++ src/test/java/com/jbc/spring/security/ldap/LdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbc.spring.security.ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
